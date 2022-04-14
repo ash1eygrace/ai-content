66bb83a0924ca7c15ad429e90fae116a5f53dee0
ima4y <ashleygrobicheau@gmail.com>
2022-04-14 10:07:32 -0600

    Add responsive menu with site title

--- copyai/src/components/Navigation.js
+++ copyai/src/components/Navigation.js
@@ -1,24 +1,26 @@
-import Nav from 'react-bootstrap/Nav';
+import { Link, Nav, Navbar, Container} from 'react-bootstrap';
 import { Component } from 'react';
 
 class Navigation extends Component {
     render() {
         return (
             <div id="navigation">
-            <Nav defaultActiveKey="/home" as="ul">
-            <Nav.Item as="li">
-                <Nav.Link href="/">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link href="product-description">Product Description</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link href="company-bio">Company Bio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link href="blog-intro">Blog Intro</Nav.Link>
-            </Nav.Item>
-            </Nav>
+                <Navbar expand="md">
+                <Container>
+                    <Nav>
+                    <Navbar.Brand href="/">AI Content Generator</Navbar.Brand>
+                    </Nav>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse>
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="product-description">Product Description</Nav.Link>
+                        <Nav.Link href="company-bio">Company Bio</Nav.Link>
+                        <Nav.Link href="blog-intro">Blog Intro</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+                </Navbar>
             </div>
         )
     }
