189f3d236b53157f2a42f5c4d5c2e5ddb4c5dd95
ima4y <ashleygrobicheau@gmail.com>
2022-04-14 11:21:31 -0600

    Add columns and cards to homepage

--- copyai/src/components/Home.js
+++ copyai/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Component } from 'react'
+import { Container, Card, Row, Col } from 'react-bootstrap'
 
 class Home extends Component {
     render() {
@@ -7,6 +8,44 @@ class Home extends Component {
             <div>
                 <h1>Home Page</h1>
                 <p>Some content will go here</p>
+                <Container>
+                <Row>
+                    <Col>
+                    <Card className="text-center">
+                        <Card.Header><h2>Product Description</h2></Card.Header>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                            <p>Generate an awesome Product Description</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card className="text-center">
+                        <Card.Header><h2>Company Bio</h2></Card.Header>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                            <p>Generate an awesome Company Bio</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    <Col>
+                    <Card className="text-center">
+                        <Card.Header><h2>Blog Intro</h2></Card.Header>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                            <p>Generate an awesome Blog Into Paragraph</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                </Row>
+                </Container>
+
             </div>
         )
     }
