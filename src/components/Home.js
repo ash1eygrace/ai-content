3f12b36c3764c3535368a0186ecd8571e4421374
ash1eygrace <ashleygrobicheau@gmail.com>
2022-06-04 11:24:13 -0600

    import image from assets/images into home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Component } from 'react'
 import { Container, Card, Row, Col, Button } from 'react-bootstrap'
-import Logo from '../ash-photo.png'
+import Logo from '../assets/images/ash-photo.png'
 
 class Home extends Component {
     render() {
