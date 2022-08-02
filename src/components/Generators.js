3b6f2034991ec25f75dcb6bb105b1efbfbf1e8e5
ash1eygrace <ashleygrobicheau@gmail.com>
2022-08-02 07:17:07 -0600

    new generators page with all apps

---
+++ src/components/Generators.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Component } from 'react'
+import { Container, Card, Row, Col, Button } from 'react-bootstrap'
+
+class Generators extends Component {
+render() {
+    return (
+        <Container>
+        <Row>
+        <Col sm={6}>
+        <Card className="text-center">
+            <Card.Header><h2>Product Description</h2></Card.Header>
+            <Card.Body>
+                <Card.Text>
+                <p>Generate an awesome Product Description</p>
+                <Button href="product-description">Product Description Generator</Button>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={6}>
+        <Card className="text-center">
+            <Card.Header><h2>Company Bio</h2></Card.Header>
+            <Card.Body>
+                <Card.Text>
+                <p>Generate an awesome Company Bio</p>
+                <Button href="company-bio">Company Bio Generator</Button>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={6}>
+        <Card className="text-center">
+            <Card.Header><h2>Blog Intro</h2></Card.Header>
+            <Card.Body>
+                <Card.Text>
+                <p>Generate an awesome Blog Into Paragraph</p>
+                <Button href="blog-intro">Blog Intro Generator</Button>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={6}>
+        <Card className="text-center">
+            <Card.Header><h2>LinkedIn Job Description</h2></Card.Header>
+            <Card.Body>
+                <Card.Text>
+                 <p>Generate a knowledgeable LinkedIn Job Description</p>
+                <Button href="li-job-description">Job Description Generator</Button>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+    </Row>
+    </Container>
+    )    
+}
+}
+export default Generators;
