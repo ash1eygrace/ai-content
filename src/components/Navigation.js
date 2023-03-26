35b24a114a338f6ea2ea7ab8b93805895231539c
ash1eygrace <ashleygrobicheau@gmail.com>
2023-03-26 14:19:06 -0600

    refactor navigation component from class to functional

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,23 @@
-import { Nav, Navbar, Container} from 'react-bootstrap';
-import React, { Component }  from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import React, { Component } from 'react';
 
-class Navigation extends Component {
-    render() {
-        return (
-            <div id="navigation">
-                <Navbar expand="md">
-                <Container>
-                    <Nav>
-                    <Navbar.Brand href="/">AshAI</Navbar.Brand>
-                    </Nav>
-                    <Navbar.Toggle/>
-                    <Navbar.Collapse>
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="generators">Generators</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-                </Navbar>
-            </div>
-        )
-    }
-}
-
-export default Navigation;
+const Navigation = React.memo(() => (
+    <div id="navigation">
+      <Navbar expand="md">
+        <Container>
+          <Nav>
+            <Navbar.Brand href="/">AshAI</Navbar.Brand>
+          </Nav>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="ml-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="generators">Generators</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  ));
+  
+  export default Navigation;
