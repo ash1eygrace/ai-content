f26fdddb23c6515b432cd3973f963b4a1c03ab5a
ash1eygrace <ashleygrobicheau@gmail.com>
2023-03-26 14:26:15 -0600

    run linter on navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,21 +3,21 @@ import React, { Component } from 'react';
 
 const Navigation = React.memo(() => (
     <div id="navigation">
-      <Navbar expand="md">
-        <Container>
-          <Nav>
-            <Navbar.Brand href="/">AshAI</Navbar.Brand>
-          </Nav>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav className="ml-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="generators">Generators</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar expand="md">
+            <Container>
+                <Nav>
+                    <Navbar.Brand href="/">AshAI</Navbar.Brand>
+                </Nav>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="generators">Generators</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </div>
-  ));
-  
-  export default Navigation;
+));
+
+export default Navigation;
