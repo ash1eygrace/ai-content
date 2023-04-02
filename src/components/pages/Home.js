38322bddb9243f2ac1054443bb7567de2288e4cd
ash1eygrace <ashleygrobicheau@gmail.com>
2023-04-02 08:01:24 -0600

    fix className console error in Home.js and main page structure

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,9 +8,9 @@ const Home = () => {
     const featuredGenerators = generatorList.filter((generator) => generator.featured);
 
     return (
-        <div>
+        <div id='main'>
             <Container id="hero">
-                <img src={Logo} class="logo" alt='Illustration of woman with long brown hair and purple back ground' />
+                <img src={Logo} className="logo" alt='Illustration of woman with long brown hair and purple back ground' />
                 <h1>Hey, I’m AshAI.</h1>
                 <p>I'm a sales and content virtual assistant. You'll never have to think about what to say again. With a click of a button I'll write high quality copy for your products, company bio, or blog intro paragraphs. I was trained via OpenAI and achieved my GPT-3 degree.</p>
                 <h3>What I can do for you:</h3>
