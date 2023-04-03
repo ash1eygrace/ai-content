6d997c5c3f410009c81ac14b61b81a489f8df45a
ash1eygrace <ashleygrobicheau@gmail.com>
2023-04-03 15:43:39 -0600

    use usereduce in generator component to manage state and actions

--- src/components/generators/Generator.js
+++ src/components/generators/Generator.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { Container, Form, Button, Card, Row, Col } from 'react-bootstrap'
-import {callAPI} from '../api/OpenAIAPI.js';
-
+import { callAPI } from '../api/OpenAIAPI.js';
 
 import CopyToClipboard from '../common/CopyToClipboard.js';
 import LoadingSpinner from '../common/LoadingSpinner.js';
 
-const GeneratorComponent = (props) => {
-  const [heading, setHeading] = useState('AI Generated Response:');
-  const [response, setResponse] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+const initialState = {
+  heading: 'AI Generated Response:',
+  response: '',
+  errorMessage: '',
+  dataLoaded: false,
+  isFormSubmitted: false,
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_HEADING':
+      return { ...state, heading: action.payload };
+    case 'SET_RESPONSE':
+      return { ...state, response: action.payload };
+    case 'SET_ERROR_MESSAGE':
+      return { ...state, errorMessage: action.payload };
+    case 'SET_DATA_LOADED':
+      return { ...state, dataLoaded: action.payload };
+    case 'SET_IS_FORM_SUBMITTED':
+      return { ...state, isFormSubmitted: action.payload };
+    default:
+      return state;
+  }
+}
+
+const GeneratorComponent = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -20,23 +39,24 @@ const GeneratorComponent = (props) => {
     const formData = new FormData(e.target);
     const prompt = `${props.generatorData.prompt}${formData.get(props.generatorData.formName)}`;
 
-    setHeading(`Thinking about your ${props.generatorData.title} now...`);
-    setResponse('');
-    setErrorMessage('');
-    setIsFormSubmitted(true);
-    setDataLoaded(false);
+    dispatch({ type: 'SET_HEADING', payload: `Thinking about your ${props.generatorData.title} now...` });
+    dispatch({ type: 'SET_RESPONSE', payload: '' });
+    dispatch({ type: 'SET_ERROR_MESSAGE', payload: '' });
+    dispatch({ type: 'SET_IS_FORM_SUBMITTED', payload: true });
+    dispatch({ type: 'SET_DATA_LOADED', payload: false });
     callAPI(prompt).then((data) => {
       if (data.error) {
-        setErrorMessage(data.message);
+        dispatch({ type: 'SET_ERROR_MESSAGE', payload: data.message });
       } else {
-        setHeading(`Your AI Generated ${props.generatorData.title}:`);
-        setResponse(data);
+        dispatch({ type: 'SET_HEADING', payload: `Your AI Generated ${props.generatorData.title}:` });
+        dispatch({ type: 'SET_RESPONSE', payload: data });
       }
-      setDataLoaded(true);
+      dispatch({ type: 'SET_DATA_LOADED', payload: true });
     });
   };
 
   const { title, description2, formLabel, formName, placeholder } = props.generatorData;
+
 
   return (
     <div id="main">
@@ -59,35 +79,34 @@ const GeneratorComponent = (props) => {
           <Col xs={12} md={8}>
             <Card className="text-center">
               <Card.Header>
-                <h2>{heading}</h2>
+                <h2>{state.heading}</h2>
               </Card.Header>
               <Card.Body>
                 {
-                errorMessage ? 
-                (
-                  <p variant="danger" className="mt-3">{errorMessage}</p>
-                ) 
-                : dataLoaded && response ? 
-                (
-                  <div className="response-container">
-                    <Card.Text>
-                      <p className="pre-wrap">{response}</p>
-                    </Card.Text>
-                    {response && <CopyToClipboard text={response} />}
-                  </div>
-                ) 
-                : !isFormSubmitted ? 
-                (
-                  <Card.Text>
-                    The Response from the AI for your {title} will show here.
-                  </Card.Text>
-                ) 
-                : 
-                (
-                  <LoadingSpinner />
-                )}
+                  state.errorMessage ?
+                    (
+                      <p variant="danger" className="mt-3">{state.errorMessage}</p>
+                    )
+                    : state.dataLoaded && state.response ?
+                      (
+                        <div className="response-container">
+                          <Card.Text>
+                            <p className="pre-wrap">{state.response}</p>
+                          </Card.Text>
+                          {state.response && <CopyToClipboard text={state.response} />}
+                        </div>
+                      )
+                      : !state.isFormSubmitted ?
+                        (
+                          <Card.Text>
+                            The Response from the AI for your {title} will show here.
+                          </Card.Text>
+                        )
+                        :
+                        (
+                          <LoadingSpinner />
+                        )}
               </Card.Body>
-
             </Card>
           </Col>
         </Row>
